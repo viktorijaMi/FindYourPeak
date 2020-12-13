1cc88817d603880fd0dbb7737c0efb5b8f1566fe
viktorijaMi <mijalchevaviktorija@gmail.com>
2020-12-13 13:08:32 +0100

    Updated the User Controller

--- find-your-peak-server/src/main/java/findyourpeak/controller/UserController.java
+++ find-your-peak-server/src/main/java/findyourpeak/controller/UserController.java
@@ -8,8 +8,27 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/")
 public class UserController {
 
+    private final UserRepository userRepository;
 
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-}
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        User user = this.userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("#" + id + "user does not exist"));
+        return ResponseEntity.ok(user);
+    }
+ }
