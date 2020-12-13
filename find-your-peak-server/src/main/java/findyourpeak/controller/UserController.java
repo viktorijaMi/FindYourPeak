7e0b0c85bece4e1c29f87ef190de3b6bede04d5e
viktorijaMi <mijalchevaviktorija@gmail.com>
2020-12-13 10:51:50 +0100

    Changed the REST controller for User

--- find-your-peak-server/src/main/java/findyourpeak/controller/UserController.java
+++ find-your-peak-server/src/main/java/findyourpeak/controller/UserController.java
@@ -4,34 +4,12 @@ import findyourpeak.model.User;
 import findyourpeak.repository.UserRepository;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
 public class UserController {
 
-    private final UserRepository userRepository;
-
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @GetMapping("users")
-    public List<User> getAllUsers(){
-        return this.userRepository.findAll();
-    }
-
-    @GetMapping("users/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id){
-        User user = this.userRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("#" + id + " user not found"));
-        return ResponseEntity.ok(user);
-    }
 
 
 }
