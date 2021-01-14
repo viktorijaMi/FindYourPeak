69391bfe75c0b3c4f0e5e2256f9265f76cd98ef7
viktorijaMi <mijalchevaviktorija@gmail.com>
2021-01-14 23:25:37 +0100

    Changes for deployment

--- find-your-peak-client/server.js
+++ find-your-peak-client/server.js
@@ -1,3 +1,20 @@
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+ 
+// const app = express();
+ 
+// // Serve only the static files form the angularapp directory
+// app.use(express.static(__dirname + '/angular-app-heroku'));
+ 
+// app.get('/*', function(req,res) {
+ 
+// res.sendFile("index.html", {root : 'dist/angular-app-heroku/'});
+// });
+ 
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +22,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angular-app-heroku'));
+app.use(express.static(__dirname + '/angularapp'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile("index.html", {root : 'dist/angular-app-heroku/'});
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
